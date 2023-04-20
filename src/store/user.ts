586a6333704cf4a83fe5fab8d24dc65e0b9a854c
andyjjrt <andyjjrt@gmail.com>
2023-04-20 21:04:33 +0800

    chore: fix type

--- src/store/user.ts
+++ src/store/user.ts
@@ -6,45 +6,45 @@ export const useUserStore = defineStore("user", () => {
   const account = ref("");
   const userId = ref("");
 
-  /**
-   * user login
-   * @returns Promise object
-   */
-  const login = async (account: string, password: string) => {
-    const response = await fetchApi("/islam/user/login", "GET", {
-      params: {
-        account,
-        password,
-      },
-    });
-    if (response.status !== 200) {
-      const { error_code, error_message } = response.data as ErrorResponse;
-      // error sections
-      return;
-    }
-    const { user_id } = response.data as UserResponse;
-    userId.value = user_id;
-  };
+  // /**
+  //  * user login
+  //  * @returns Promise object
+  //  */
+  // const login = async (account: string, password: string) => {
+  //   const response = await fetchApi("/islam/user/login", "GET", {
+  //     params: {
+  //       account,
+  //       password,
+  //     },
+  //   });
+  //   if (response.status !== 200) {
+  //     const { error_code, error_message } = response.data as ErrorResponse;
+  //     // error sections
+  //     return;
+  //   }
+  //   const { user_id } = response.data as UserResponse;
+  //   userId.value = user_id;
+  // };
 
-  /**
-   * user register
-   * @returns Promise object
-   */
-  const register = async (account: string, password: string) => {
-    const response = await fetchApi("/islam/user/register", "POST", {
-      data: {
-        account,
-        password,
-      },
-    });
-    if (response.status !== 200) {
-      const { error_code, error_message } = response.data as ErrorResponse;
-      // error sections
-      return;
-    }
-    const { user_id } = response.data as UserResponse;
-    userId.value = user_id;
-  };
+  // /**
+  //  * user register
+  //  * @returns Promise object
+  //  */
+  // const register = async (account: string, password: string) => {
+  //   const response = await fetchApi("/islam/user/register", "POST", {
+  //     data: {
+  //       account,
+  //       password,
+  //     },
+  //   });
+  //   if (response.status !== 200) {
+  //     const { error_code, error_message } = response.data as ErrorResponse;
+  //     // error sections
+  //     return;
+  //   }
+  //   const { user_id } = response.data as UserResponse;
+  //   userId.value = user_id;
+  // };
 
-  return { account, userId, login, register };
+  return { account, userId };
 });
