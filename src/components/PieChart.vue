ce285144325e4ede8c737223262c4a8770b33836
andyjjrt <andyjjrt@gmail.com>
2023-04-22 14:35:20 +0800

    feat(Problem): RHS pannel init

---
+++ src/components/PieChart.vue
@@ -0,0 +1,43 @@
+<template>
+  <Pie id="my-chart-id" :options="chartOptions" :data="chartData" />
+</template>
+
+<script lang="ts">
+import { Pie } from "vue-chartjs";
+import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement } from "chart.js";
+
+ChartJS.register(Title, Tooltip, Legend, ArcElement);
+
+export default {
+  name: "PieChart",
+  components: { Pie },
+  data() {
+    return {
+      chartData: {
+        labels: ["Red", "Blue", "Yellow"],
+        datasets: [
+          {
+            label: "My First Dataset",
+            data: [300, 50, 100],
+            backgroundColor: [
+              "rgb(255, 99, 132)",
+              "rgb(54, 162, 235)",
+              "rgb(255, 205, 86)",
+            ],
+            hoverOffset: 4,
+          },
+        ],
+      },
+      chartOptions: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: "bottom",
+            align: "start",
+          },
+        },
+      },
+    };
+  },
+};
+</script>
--- src/components/Problem.vue
+++ src/components/Problem.vue
@@ -1,94 +1,157 @@
 <template>
-  <v-card class="pa-2">
-    <v-card-title class="font-weight-bold text-h5">{{ title }}</v-card-title>
-    <v-divider />
-    <div class="pa-4">
-      <h5 class="text-h5 mb-2">Description</h5>
-      <v-md-preview v-katex :text="description" />
-    </div>
-    <div class="pa-4">
-      <h5 class="text-h5 mb-2">Input</h5>
-      <v-md-preview v-katex :text="input" />
-    </div>
-    <div class="pa-4">
-      <h5 class="text-h5 mb-2">Output</h5>
-      <v-md-preview v-katex :text="output" />
-    </div>
-    <v-row class="pa-4">
-      <template v-for="(sample, index) in samples" :key="index">
-        <v-col cols="12" sm="6" class="d-flex flex-column">
-          <h5 class="text-h5 mb-2">Sample Input {{ index + 1 }}</h5>
-          <div class="position-relative flex-grow-1">
-            <v-code tag="pre" class="pa-2 overflow-auto h-100">
-              {{ sample.input }}
-            </v-code>
-            <v-btn
-              icon="mdi-clipboard-text"
-              class="me-1 text-disabled me-2 mt-2"
-              density="compact"
-              style="position: absolute; right: 0; top: 0"
-              variant="text"
-              @click="copy(sample.input)"
-            />
-          </div>
-        </v-col>
-        <v-col cols="12" sm="6" class="d-flex flex-column">
-          <h5 class="text-h5 mb-2">Output Input {{ index + 1 }}</h5>
-          <div class="position-relative flex-grow-1">
-            <v-code tag="pre" class="pa-2 overflow-auto h-100">
-              {{ sample.output }}
-            </v-code>
-            <v-btn
-              icon="mdi-clipboard-text"
-              class="me-1 text-disabled me-2 mt-2"
-              density="compact"
-              style="position: absolute; right: 0; top: 0"
-              variant="text"
-              @click="copy(sample.output)"
-            />
-          </div>
-        </v-col>
-      </template>
-    </v-row>
-    <div class="pa-4" v-if="hint">
-      <h5 class="text-h5 mb-2">Hint</h5>
-      <v-md-preview v-katex :text="hint" />
-    </div>
-    <div class="pa-4 d-flex flex-column">
-      <div class="mb-3 d-flex">
-        <v-select
-          label="Language"
-          :items="languages"
-          variant="solo"
-          density="compact"
-          v-model="selectedLanguage"
-          hide-details
-        />
-      </div>
-      <div class="position-relative">
-        <v-progress-linear indeterminate absolute :active="loading" />
-        <CodeMirror v-model="code" :lang="selectedLanguage" />
-      </div>
-
-      <div class="mt-3 d-flex justify-space-between">
-        <div>
-          <v-btn
-            v-if="status"
-            :color="status.type"
-            :to="`/status/${status.id}`"
-            >{{ status.name }}</v-btn
-          >
+  <v-row>
+    <v-col md="9">
+      <v-card class="pa-2">
+        <v-card-title class="font-weight-bold text-h5">{{
+          title
+        }}</v-card-title>
+        <v-divider />
+        <div class="pa-4">
+          <h5 class="text-h5 mb-2">Description</h5>
+          <v-md-preview v-katex :text="description" />
         </div>
-        <v-btn @click="submit" :loading="loading">Submit</v-btn>
-      </div>
-    </div>
-  </v-card>
+        <div class="pa-4">
+          <h5 class="text-h5 mb-2">Input</h5>
+          <v-md-preview v-katex :text="input" />
+        </div>
+        <div class="pa-4">
+          <h5 class="text-h5 mb-2">Output</h5>
+          <v-md-preview v-katex :text="output" />
+        </div>
+        <v-row class="pa-4">
+          <template v-for="(sample, index) in samples" :key="index">
+            <v-col cols="12" sm="6" class="d-flex flex-column">
+              <h5 class="text-h5 mb-2">Sample Input {{ index + 1 }}</h5>
+              <div class="position-relative flex-grow-1">
+                <v-code tag="pre" class="pa-2 overflow-auto h-100">
+                  {{ sample.input }}
+                </v-code>
+                <v-btn
+                  icon="mdi-clipboard-text"
+                  class="me-1 text-disabled me-2 mt-2"
+                  density="compact"
+                  style="position: absolute; right: 0; top: 0"
+                  variant="text"
+                  @click="copy(sample.input)"
+                />
+              </div>
+            </v-col>
+            <v-col cols="12" sm="6" class="d-flex flex-column">
+              <h5 class="text-h5 mb-2">Output Input {{ index + 1 }}</h5>
+              <div class="position-relative flex-grow-1">
+                <v-code tag="pre" class="pa-2 overflow-auto h-100">
+                  {{ sample.output }}
+                </v-code>
+                <v-btn
+                  icon="mdi-clipboard-text"
+                  class="me-1 text-disabled me-2 mt-2"
+                  density="compact"
+                  style="position: absolute; right: 0; top: 0"
+                  variant="text"
+                  @click="copy(sample.output)"
+                />
+              </div>
+            </v-col>
+          </template>
+        </v-row>
+        <div class="pa-4" v-if="hint">
+          <h5 class="text-h5 mb-2">Hint</h5>
+          <v-md-preview v-katex :text="hint" />
+        </div>
+        <div class="pa-4 d-flex flex-column">
+          <div class="mb-3 d-flex">
+            <v-select
+              label="Language"
+              :items="languages"
+              variant="solo"
+              density="compact"
+              v-model="selectedLanguage"
+              hide-details
+            />
+          </div>
+          <div class="position-relative">
+            <v-progress-linear indeterminate absolute :active="loading" />
+            <CodeMirror v-model="code" :lang="selectedLanguage" />
+          </div>
+
+          <div class="mt-3 d-flex justify-space-between">
+            <div>
+              <v-btn
+                v-if="status"
+                :color="status.type"
+                :to="`/status/${status.id}`"
+                >{{ status.name }}</v-btn
+              >
+            </div>
+            <v-btn @click="submit" :loading="loading">Submit</v-btn>
+          </div>
+        </div>
+      </v-card>
+    </v-col>
+    <v-col md="3" v-if="mdAndUp">
+      <v-card class="py-1 px-2 mb-6">
+        <v-list lines="one"  density="compact">
+          <v-list-item :title="problem._id">
+            <template v-slot:prepend>
+              <v-icon icon="mdi-identifier" />
+            </template>
+          </v-list-item>
+          <v-list-item :title="problem.time_limit + ' ms'">
+            <template v-slot:prepend>
+              <v-icon icon="mdi-timer" />
+            </template>
+          </v-list-item>
+          <v-list-item :title="problem.memory_limit + ' MB'">
+            <template v-slot:prepend>
+              <v-icon icon="mdi-memory" />
+            </template>
+          </v-list-item>
+          <v-list-item :title="problem.io_mode.io_mode">
+            <template v-slot:prepend>
+              <v-icon icon="mdi-xml" />
+            </template>
+          </v-list-item>
+          <v-list-item :title="problem.created_by.username">
+            <template v-slot:prepend>
+              <v-icon icon="mdi-shield-crown" />
+            </template>
+          </v-list-item>
+          <v-list-item>
+            <DifficultyLabel :difficulty="problem.difficulty" />
+            <template v-slot:prepend>
+              <v-icon icon="mdi-gavel" />
+            </template>
+          </v-list-item>
+          <v-list-item>
+            <v-chip
+              class="ma-1 me-2"
+              size="small"
+              label
+              color="primary"
+              v-for="tag in problem.tags"
+            >
+              {{ tag }}
+            </v-chip>
+            <template v-slot:prepend>
+              <v-icon icon="mdi-tag-multiple" />
+            </template>
+          </v-list-item>
+        </v-list>
+      </v-card>
+      <v-card class="pa-4">
+        <PieChart />
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script setup lang="ts">
 import { computed, ref, watch } from "vue";
-import vKatex from "../plugins/vKatex";
+import { useDisplay } from "vuetify";
 import CodeMirror from "../components/CodeMirror.vue";
+import PieChart from "./PieChart.vue";
+import DifficultyLabel from "./DifficultyLabel.vue";
+import vKatex from "../plugins/vKatex";
 import { fetchApi } from "../utils/api";
 import statusList from "../utils/status";
 import Message from "vue-m-message";
@@ -96,6 +159,8 @@ import Message from "vue-m-message";
 const props = defineProps<{
   problem: Problem;
 }>();
+
+const { mdAndUp } = useDisplay();
 
 const title = computed(() => props.problem?.title);
 const description = computed(() => decodeURI(props.problem.description));
