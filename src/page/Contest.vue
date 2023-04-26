4c19cbc92a260f048979be51adcc3a3ca1f11011
andyjjrt <andyjjrt@gmail.com>
2023-04-26 12:15:36 +0800

    feat(Contest): Add time counter

--- src/page/Contest.vue
+++ src/page/Contest.vue
@@ -1,7 +1,19 @@
 <template>
   <ErrorMessage :message="error" v-if="error" />
   <template v-else-if="contest">
-    <h1>{{ contest.title }}</h1>
+    <div class="d-flex justify-space-between">
+      <h1>{{ contest.title }}</h1>
+      <v-chip
+        class="ma-1 me-2"
+        size="large"
+        label
+        :color="remainTimeString.type"
+        variant="elevated"
+      >
+        {{ remainTimeString.value }}
+      </v-chip>
+    </div>
+
     <v-tabs>
       <v-tab :to="{ name: 'Contest', params: { contestId: contestId } }">
         Overview
@@ -30,15 +42,18 @@
 
 <script setup lang="ts">
 import { useRoute } from "vue-router";
-import { ref, onMounted, provide, readonly } from "vue";
+import { ref, onMounted, provide, readonly, computed } from "vue";
 import { fetchApi } from "../utils/api";
 import ErrorMessage from "../components/ErrorMessage.vue";
+import { time } from "console";
 
 const routes = useRoute();
 const contest = ref<Contest | null>(null);
 const loading = ref(false);
 const error = ref<string | null>(null);
 const contestId = routes.params.contestId;
+const remainTime = ref(0);
+const timer = ref(0);
 
 const init = async () => {
   loading.value = true;
@@ -49,8 +64,34 @@ const init = async () => {
   });
   loading.value = false;
   if (response.data.error) error.value = response.data.data;
-  else contest.value = response.data.data;
+  else {
+    contest.value = response.data.data;
+    remainTime.value = Math.round(
+      (new Date(contest.value!.end_time).getTime() - new Date().getTime()) /
+        1000
+    );
+    timer.value = window.setInterval(() => {
+      const time = Math.round(
+        (new Date(contest.value!.end_time).getTime() - new Date().getTime()) /
+          1000
+      );
+      if (time < 0) window.clearInterval(timer.value);
+      else remainTime.value = time;
+    }, 1000);
+  }
 };
+
+const remainTimeString = computed(() => {
+  if (!contest.value) return { type: undefined, value: undefined };
+  if (remainTime.value < 0) return { type: "error", value: "Ended" };
+  if (new Date() < new Date(contest.value.start_time))
+    return { type: "warning", value: "Not Started" };
+  const seconds = Math.floor(remainTime.value % 60);
+  const minutes = Math.floor(remainTime.value / 60) % 60;
+  const hours = Math.floor((remainTime.value / (60 * 60)) % 24);
+  const days = Math.floor(remainTime.value / (60 * 60 * 24));
+  return { type: "success", value: `${days}D ${hours}:${minutes}:${seconds}` };
+});
 
 provide("contest", readonly(contest));
 
