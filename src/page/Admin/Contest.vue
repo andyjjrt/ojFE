4f77403cf4f82dde0e9a56a560a18b428ec1cad7
andyjjrt <andyjjrt@gmail.com>
2023-12-31 10:40:35 +0800

    fix: Add preview destination

--- src/page/Admin/Contest.vue
+++ src/page/Admin/Contest.vue
@@ -3,7 +3,7 @@
     <div class="d-flex justify-space-between align-center pb-3">
       <v-skeleton-loader type="heading" v-if="contest.title === ''" min-width="300"></v-skeleton-loader>
       <h1 v-else>{{ contest.title }}</h1>
-      <v-btn>Preview</v-btn>
+      <v-btn :to="{ name: 'Contest', params: { contestId: contestId } }">Preview</v-btn>
     </div>
     <v-tabs class="mb-3">
       <v-tab :to="{ name: 'AdminContest', params: { contestId: contestId } }">
