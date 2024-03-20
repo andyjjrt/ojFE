d8826fbd646733423d8fc958cae14ce87d2f3993
andyjjrt <andyjjrt@gmail.com>
2024-03-20 20:05:17 +0800

    fix: Set default color

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -44,6 +44,9 @@ export default createVuetify({
     VDialog: {
       transition: "scroll-y-transition",
     },
+    VSwitch: {
+      color: "primary",
+    },
     VTextField: {
       color: "primary",
     },
